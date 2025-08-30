b36cace29ddf1f59d03cc70bb8772cac4e31ee92
seewol <jeeneepark@naver.com>
2025-08-30 20:20:22 +0900

    feat: OrderController-test

--- src/main/java/com/jeeeun/demo/controller/OrderController.java
+++ src/main/java/com/jeeeun/demo/controller/OrderController.java
@@ -1,10 +1,15 @@
 package com.jeeeun.demo.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RequiredArgsConstructor
 @RestController
 public class OrderController {
 
+    @GetMapping("/orders")
+    public String test() {
+        return "why..";
+    }
 }
